5fddfa3e86fafa6a6f0bf7d14dc310ceb7476ea0
khiemnd777 <khiemnd777@gmail.com>
2025-11-12 20:58:54 +0700

    rename flyway version

--- flyway/sql/V3__init_admin.sql
+++ flyway/sql/V3__init_admin.sql
@@ -1,16 +1,16 @@
 -- 1. Tạo user admin nếu chưa có
 INSERT INTO users (email, password, name, active, avatar, provider, provider_id, phone)
 SELECT 
-  'admin@luca.vn',
+  'khiemnd777@gmail.com',
   '$2a$10$W4WXXgBmh4mKSgfRpHmEsusc2x7t7JP3E5ZcrK0k2k2liIEFjTCE2', -- bcrypt: "sa"
-  'Admin',
+  'Administrator',
   true,
   'https://api.dicebear.com/9.x/initials/svg?seed=Admin',
   NULL,
   NULL,
   NULL
 WHERE NOT EXISTS (
-  SELECT 1 FROM users WHERE email = 'admin@luca.vn'
+  SELECT 1 FROM users WHERE email = 'khiemnd777@gmail.com'
 );
 
 -- 2. Gán user admin ↔ role admin
@@ -18,5 +18,5 @@ INSERT INTO user_roles (user_id, role_id)
 SELECT u.id, r.id
 FROM users u
 JOIN roles r ON r.role_name = 'admin'
-WHERE u.email = 'admin@luca.vn'
+WHERE u.email = 'khiemnd777@gmail.com'
 ON CONFLICT DO NOTHING;
